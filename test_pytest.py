e8d03e8a8de4e6a8b0eb8ff0ba51a46b4ebf001e
happyyuyu <zhouyh1997@gmail.com>
2018-06-15 17:36:37 -0700

    more testing with fixtures

--- test_pytest.py
+++ test_pytest.py
@@ -1,2 +1,20 @@
 import pytest
 
+
+class TestClass(object):
+    @pytest.fixture(scope="class")
+    
+    def start(self):
+        '''initialize fixture'''
+        print("Fixture started")
+        return 10
+
+    def test_one(self, start):
+        # x = "this"
+        print("log test one")
+        assert start==10
+
+    def test_two(self, start):
+        # x = "hello"
+        # print("log test TWO!!!!!!!!!!!!")
+        assert start==2
